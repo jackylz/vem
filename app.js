f16de1d9424a6e94abfc182413e980e440b313be
jackylz <zhen.lin@renren-inc.com>
2016-01-05 20:13:11 +0800

    add gps module

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
  * for vem
  * lin@2016.01.03
  */
+'use strict'
 
 var http = require('http'),
     express = require('express'),
@@ -12,23 +13,58 @@ var http = require('http'),
 function reqEntry(req,res){
     
     if(req.url.substr(0,12) == '/getLocation' ){
-    	var loc = ["111","121"];
+    	var mLoc = {
+	    	jiao2 : {
+	    		name : "教二大厅",
+	    		loc  : ['39.98837981','116.37682346']
+	    	},
+			jiao3 : {
+	    		name : "教三大厅",
+	    		loc  : ['39.98842723','116.37680759']
+			},
+			xue4 : {
+	    		name : "学四公寓",
+	    		loc  : ['39.98841378','116.37683599']
+			}
+		};
+
     	var ele = urltool.parse(req.url,true).query;
     	var userLoc = ele.nowLoc.split(",");
 
-    	console.log(userLoc);
-    	getNear(loc,userLoc);
+    	console.log("userLoc:",userLoc);
+    	var tempNear = getNear(mLoc,userLoc) + "-BISTU";
     	res.writeHead(200,{"Content-Type":"application/json"});
-    	res.end(JSON.stringify({code:0,near:"JIAO 2 - BISTU"}));
+    	res.end(JSON.stringify({code:0,near:tempNear}));
     }
 
 }
 function getNear(locA,locB,callback){
 
-	var disX = +locA[0] - +locB[0];
-	var disY = +locA[1] - locB[1];
-	var disLoc = Math.sqrt(disX*disX + disY*disY);
-	console.log((+disLoc).toFixed(2)) ;
+	var locArr = [],
+		nameArr = [],
+		disX = null,
+		disY = null,
+		disLoc = null;
+
+	Object.keys(locA).forEach(function(n,i){
+		console.log(locA[n].loc);
+		var t = locA[n].loc;
+		var n = locA[n].name;
+		disX = +t[0] - +locB[0];
+		disY = +t[1] - +locB[1];
+		disLoc = Math.sqrt(disX*disX + disY*disY);
+		locArr.push(disLoc);
+		nameArr.push(n);
+	});
+	console.log(locArr);
+	console.log(nameArr);
+	var indexArr = 0;
+	for(var i=0;i<locArr.length;i++){
+		if(locArr[indexArr]>locArr[i]){
+			indexArr = i;
+		}
+	}
+	return nameArr[indexArr];
 }
 var s = http.createServer(reqEntry);
 s.listen(8080);
--- static/js/index.js
+++ static/js/index.js
@@ -35,19 +35,30 @@
 	    	},
 
 	    	getLocation:function(){
-	    		$.ajax({
-	    			url:"http://www.dormstore.cn/getLocation",
-	    			type:"get",
-	    			dataType:"json",
-	    			data:{
-	    				nowLoc:"31,111"
-	    			},
-	    			success:function(r){
-	    				console.log(r);
-	    				$('.locate-info').text(r.near);
-	    			}
-	    		});
-	    	}
+	    		var nowLoc = "";
+	    		if(navigator.geolocation){
+        			navigator.geolocation.getCurrentPosition(function (position) {
+        				var lat = position.coords.latitude;
+            			var lon = position.coords.longitude;
+            			console.log(lat+","+lon);
+            			nowLoc = lat.toFixed(8) + "," + lon.toFixed(8);
+            			$.ajax({
+			    			url:"http://www.dormstore.cn/getLocation",
+			    			type:"get",
+			    			dataType:"json",
+			    			data:{
+			    				nowLoc:nowLoc
+			    			},
+			    			success:function(r){
+			    				console.log(r);
+			    				$('.locate-info').text(r.near);
+			    			}
+			    		});
+        			});
+        		}
+
+	    		
+	    	}			
 	    }
 	});
 })(jQuery);
